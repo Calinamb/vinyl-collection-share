c617de67801e5a4affabaf65115b2451a3d608b5
Calina Michelle <CalinaMichelle@Maccy-Mcqueen.local>
2026-03-03 22:08:32 +0100

    Fix client modules and render users list

--- public/app.mjs
+++ public/app.mjs
@@ -1,5 +1,7 @@
+import UsersViewController from "./controllers/usersController.mjs";
 import { get } from "./modules/fetchManager.mjs";
-import { UsersController } from "./controllers/usersController.mjs";
 
 const users = await get("/users");
-new UsersController(document.querySelector("#users"), users);
+
+new UsersViewController(document.querySelector("#users"),users
+);
